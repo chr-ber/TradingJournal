cf8116e6af89539dfde8e74cbc11103079636869
John Doepamine <christopher.alexander.berger@gmail.com>
2022-04-19 22:12:46 +0200

    cleaned up navigatiomanager

--- src/Application/Common/Extensions/NavigationManagerExtensions.cs
+++ src/Application/Common/Extensions/NavigationManagerExtensions.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.WebUtilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TradingJournal.Application.Common.Extensions;
 
